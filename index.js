3a8b10eccca95e4efe162ab4c3a8fdc159b07c2f
Pratyush Vatsal Shukla <thelaidoffengineer@gmail.com>
2024-07-07 10:33:51 +0530

    LEarn to create basic API endpoints

--- index.js
+++ index.js
@@ -5,13 +5,35 @@ const fs = require('fs');
 const jsonD = fs.readFileSync('./fakeJson.json');
 
 const express = require('express') ;
+const { type } = require('os');
 const server = express();
 
+
+//APIs / Endpoints / Routes / But not REST APIs
 server.get('/',(req,res)=>{
     // res.send("<i>Hello from Express</i>") ; // sends either text or entire html
     // res.sendFile(__dirname+'/hello.txt') ; // sends file
-    res.json(JSON.parse(jsonD)) ; // send express data
+
+    // res.sendStatus(404)
+    // res.json(JSON.parse(jsonD)) ; // send express data
+
+    // res.status(201).send("Hello From Express JS") // Using Shorthand
+
+res.json({type:'GET'})
 })
+
+server.post('/',(req, res)=>{
+    res.json({type:'POST'})
+}) ;
+
+server.put('/',(req, res)=>{
+    res.json({type:'PUT'})
+}) ;
+      
+server.delete('/',(req, res)=>{
+    res.json({type:'DELETE'})
+}) ;
+
 
 
 server.listen(3000, '127.0.0.1',()=>{
